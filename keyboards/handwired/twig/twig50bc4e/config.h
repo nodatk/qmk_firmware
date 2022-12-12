5134846a721a2aff193e547cd533d86f295936a6
nodatk <nodatk@gmail.com>
2022-12-12 23:14:55 +0900

    Upstream fixup

--- keyboards/handwired/twig/twig50bc4e/config.h
+++ keyboards/handwired/twig/twig50bc4e/config.h
@@ -3,22 +3,6 @@
 
 #pragma once
 
-#include "config_common.h"
-
-/* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 14
-
-#define MATRIX_ROW_PINS { B9, B6, A4, B8 }
-#define MATRIX_COL_PINS { A7, A6, A5, A1, B13, B14, B15, B1, C14, C15, A3, B10, A8, B0}
-
-#define DIODE_DIRECTION COL2ROW
-
-#define DEBOUNCE 8
-#define TAPPING_TERM 150
-
-#define FORCE_NKRO
-
 #ifdef AUDIO_ENABLE
   #define AUDIO_PIN A0
   #define AUDIO_PIN_ALT B3
--- keyboards/handwired/twig/twig50bc4e/twig50bc4e.c
+++
@@ -1,4 +0,0 @@
-// Copyright 2022 Takeshi Noda (nodatk@gmail.com)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#include "twig50bc4e.h"
